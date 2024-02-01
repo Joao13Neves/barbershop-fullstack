284a7e05f36374a167085329af5f525e892a390c
unknown <you@example.com>
2024-02-01 10:23:01 -0300

    feat: add header component initial

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -6,8 +6,8 @@ import { MenuIcon } from "lucide-react";
 const Header = () => {
   return (
     <Card>
-      <CardContent className="p-5 justify-between itens-center flex flex-row">
-        <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+      <CardContent className="p-5 justify-between items-center flex flex-row">
+        <Image src="/logo.png" alt="FSW Barber" height={18} width={120}/>
         <Button variant={"outline"} size={"icon"}>
           <MenuIcon size={18}></MenuIcon>
         </Button>
