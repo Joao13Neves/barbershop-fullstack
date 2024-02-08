50d0bdc0960884ed3fd6f5132b68c796e5de2b55
unknown <you@example.com>
2024-02-07 23:03:08 -0300

    feat: add footer component and populares menu

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,14 +1,48 @@
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Barbershop } from "@prisma/client";
+import Image from "next/image";
+import { Button } from "@/app/_components/ui/button";
+import { Badge } from "@/app/_components/ui/badge";
+import { StarIcon } from "lucide-react";
+import { Span } from "next/dist/trace";
 
 interface BarbershopItemProps {
   barbershop: Barbershop;
 }
 const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
   return (
-    <Card>
-      <CardContent className="p-0">
-        <h1>{barbershop.name}</h1>
+    <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
+      <CardContent className="px-1 py-0">
+        <div className=" w-full h-[159px] relative">
+          <div className="absolute top-2 left-2 z-50">
+            <Badge variant="secondary" className="opacity-90 flex gap-1 items-center top-3 left-3">
+              <StarIcon size={16} className="fill-primary text-secondary"></StarIcon>
+              <span className="text-xs">5,0</span>
+            </Badge>
+          </div>
+          <Image
+            src={barbershop.imageUrl}
+            fill
+            alt={barbershop.name}
+            height={0}
+            width={0}
+            sizes="100vw"
+            style={{ objectFit: "cover" }}
+            className="rounded-2xl"
+          />
+        </div>
+
+        <div className="px-2 pb-3">
+          <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">
+            {barbershop.name}
+          </h2>
+          <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">
+            {barbershop.address}
+          </p>
+          <Button className="w-full mt-4" variant="secondary">
+            Agendar
+          </Button>
+        </div>
       </CardContent>
     </Card>
   );
