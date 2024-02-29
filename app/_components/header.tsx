a9e5a4a62aa5deb3f2ec1b564e5712d0f54f1043
unknown <you@example.com>
2024-02-28 22:40:02 -0300

    feat: add side-menu

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,29 +3,11 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
-import {
-  CalendarIcon,
-  HomeIcon,
-  Link,
-  LogInIcon,
-  LogOutIcon,
-  MenuIcon,
-  UserIcon,
-} from "lucide-react";
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "./ui/sheet";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { Avatar, AvatarImage } from "./ui/avatar";
-const Header = () => {
-  const { data, status } = useSession();
+import { MenuIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import SideMenu from "./side-menu";
 
-  const handleLogOutClick = () => signOut();
-  const handleLogInClick = () => signIn("google");
+const Header = () => {
 
   return (
     <Card>
@@ -37,58 +19,9 @@ const Header = () => {
               <MenuIcon size={18}></MenuIcon>
             </Button>
           </SheetTrigger>
-
+            
           <SheetContent className="p-0">
-            <SheetHeader className="text-left border-b border-solid border-secundary p-5">
-              <SheetTitle>Menu</SheetTitle>
-            </SheetHeader>
-
-            {data?.user ? (
-              <div className="flex justify-between px-5 py-6 items-center">
-                <div className="flex items-center gap-3">
-                  <Avatar>
-                    <AvatarImage src={data.user?.image ?? ""} />
-                  </Avatar>
-
-                  <h2 className="font">{data.user?.name}</h2>
-                </div>
-
-                <Button variant="secondary" size="icon">
-                  <LogOutIcon onClick={handleLogOutClick}></LogOutIcon>
-                </Button>
-              </div>
-            ) : (
-              <div className="flex flex-col px-5 py-6 gap-3">
-                <div className="flex items-center gap-2">
-                  <UserIcon></UserIcon>
-                  <h2 className="font-bold">Olá, faça seu login!</h2>
-                </div>
-                <Button
-                  onClick={handleLogInClick}
-                  variant="secondary"
-                  className="w-full justify-start"
-                >
-                  <LogInIcon className="mr-2" size={18}></LogInIcon>
-                  Fazer Login
-                </Button>
-              </div>
-            )}
-
-            <div className="flex flex-col gap-3 px-5" >
-              <Button variant="outline" className="justify-start" asChild>
-                <Link href="/">
-                  <HomeIcon className="mr-2"  size={18}></HomeIcon>
-                  Início
-                </Link>
-              </Button>
-
-              {data?.user && (
-                <Button variant="outline" className="justify-start">
-                  <CalendarIcon className="mr-2" size={18}></CalendarIcon>
-                  Agendamentos
-                </Button>
-              )}
-            </div>
+          <SideMenu />
           </SheetContent>
         </Sheet>
       </CardContent>
